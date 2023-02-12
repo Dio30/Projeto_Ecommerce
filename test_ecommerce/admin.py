20f9aa758b2ee8b81d56206c39618b8c70207237
Dione <padilhadione1@gmail.com>
2023-02-12 14:05:48 -0300

    add site responsivo3

--- test_ecommerce/admin.py
+++ test_ecommerce/admin.py
@@ -20,5 +20,5 @@ admin.site.register(Pedido, PedidoAdmin)
 
 class PedidoItemAdmin(admin.ModelAdmin):
     readonly_fields = ['produto', 'quantidade', 'pedido']
-    list_display = ('pedido', 'quantidade', 'produto', 'pegar_total')
+    list_display = ('pedido', 'quantidade', 'produto', 'pegar_total', 'data_adicao')
 admin.site.register(PedidoItem, PedidoItemAdmin)
